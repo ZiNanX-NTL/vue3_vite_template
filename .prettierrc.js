a7628f1402eb84a4e148df75b2faaa5c2f026ffc
ZiNanX-NTL <702668400@qq.com>
2022-05-03 22:32:20 +0800

    '登陆页面,组件自动注册'

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   tabWidth: 2,
   jsxSingleQuote: true,
   jsxBracketSameLine: true,
-  printWidth: 100,
+  printWidth: 150,
   singleQuote: true,
   semi: false,
   endOfLine: 'auto',
